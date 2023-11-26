29b4665bdb82bf3f6abfbac12dc72c4de764b3df
ajayawachar400@gmail.com <ajay.awachar@udchalo.com>
2023-11-26 17:31:12 +0530

    episod1 changes

---
+++ App.js
@@ -0,0 +1,23 @@
+/* 
+<div id="parent">
+    <div id="child">
+        <h1>i am h1</h1>
+        <h1>i am h2</h1>
+    </div>
+    <div id="child2">
+        <h1>i am h1</h1>
+        <h1>i am h2</h1>
+    </div>
+</div> 
+*/
+
+const nestedElements = React.createElement('div', { id: "parent" }, [
+    React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'I am Heading'), React.createElement('h1', {}, 'I am Heading')]),
+    React.createElement('div', { id: 'child1' }, [React.createElement('h1', {}, 'I am Heading'), React.createElement('h1', {}, 'I am Heading')])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(nestedElements);
+
+root.render(nestedElements);
