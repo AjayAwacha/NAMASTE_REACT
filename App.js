98a48b172dbf42dc38df151db3a6c21ac792f3ee
ajayawachar400@gmail.com <ajay.awachar@udchalo.com>
2023-11-26 23:06:20 +0530

    episod 2 parcel

--- App.js
+++ App.js
@@ -11,6 +11,9 @@
 </div> 
 */
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const nestedElements = React.createElement('div', { id: "parent" }, [
     React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'I am Heading'), React.createElement('h1', {}, 'I am Heading')]),
     React.createElement('div', { id: 'child1' }, [React.createElement('h1', {}, 'I am Heading'), React.createElement('h1', {}, 'I am Heading')])
---
+++ README.md
@@ -0,0 +1,9 @@
+# React
+
+# Parcel
+- create Dev Build
+- run local server
+- Hot Reloading(when we change in file automatically display on UI)
+- caching
+- Image optimization
+- compress and minified file for production build
